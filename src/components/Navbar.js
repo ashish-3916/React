aff03e48c78dda59e3cd1290b64faba810b8b3f5
ashish-3916 <dangiashish1618@gmail.com>
2022-01-26 23:24:52 +0530

    done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,27 @@
 import React from "react";
 import PropTypes from "prop-types";
-
+import {Link} from "react-router-dom";
 export default function Navbar(props) {
   return (
     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">
+        <Link className="navbar-brand" to="/">
           {props.name}
-        </a>
+        </Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">
+              <Link className="nav-link active" aria-current="page" to="/home">
                 Home
-              </a>
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/">
+              <Link className="nav-link" to="/about">
                 {props.about}
-              </a>
+              </Link>
             </li>
           </ul>
           <div className={`form-check form-switch text-${props.mode==="light"?"black":"white"}`}>
